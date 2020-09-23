085e885ac8c0863c99ebadbb8991babbcf949125
cupjoo <cupjoo@naver.com>
2020-09-23 14:57:32 +0900

    update EventController tests to integration test

--- src/test/java/com/example/springrestapi/event/EventControllerTests.java
+++ src/test/java/com/example/springrestapi/event/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.example.springrestapi.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -35,6 +36,7 @@ public class EventControllerTests {
     @Test
     public void createEvent() throws Exception {
         Event event = Event.builder()
+                .id(1000)
                 .name("Spirng")
                 .description("REST API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2020, 9, 23, 12, 38))
@@ -46,7 +48,6 @@ public class EventControllerTests {
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타트업 팩토리")
                 .build();
-        event.setId(10);
 
         Mockito.when(eventRepository.save(event)).thenReturn(event);
 
@@ -58,6 +59,7 @@ public class EventControllerTests {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("id").exists())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(1000)));
     }
 }
